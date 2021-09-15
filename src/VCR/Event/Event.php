9134c7228d8f2c754bf6111c7f5f56b56374f4ea
phpjit <contact@taciclei.com>
2021-09-15 17:01:51 +0200

    Update Event.php
    hotfix behat event

--- src/VCR/Event/Event.php
+++ src/VCR/Event/Event.php
@@ -4,8 +4,13 @@ namespace VCR\Event;
 
 use Symfony\Component\EventDispatcher\Event as LegacyEvent;
 use Symfony\Contracts\EventDispatcher\Event as ContractsEvent;
+use Behat\Testwork\Event\Event as BehatEvent;
 
-if (class_exists(ContractsEvent::class) && !class_exists(LegacyEvent::class)) {
+if (class_exists(BehatEvent::class)) {
+    abstract class SymphonyEvent extends BehatEvent
+    {
+    }
+}elseif (class_exists(ContractsEvent::class) && !class_exists(LegacyEvent::class)) {
     abstract class SymphonyEvent extends ContractsEvent
     {
     }
